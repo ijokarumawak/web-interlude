7d871a5facdad22128787a182d50b01c4a767444
Koji Kawamura <ijokarumawak@gmail.com>
2023-09-04 15:13:24 +0900

    Add examples.

---
+++ src/app/video-playlist/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useRef, useEffect } from "react";
+import videojs from "video.js";
+import videojsPlaylistPlugin from "videojs-playlist";
+import "video.js/dist/video-js.css";
+
+videojs.registerPlugin('playlist', videojsPlaylistPlugin);
+
+export default function Video() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const player = videojs(videoRef.current)
+      console.log('player', player)
+
+      player.playlist([{
+        sources: [{
+          src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+          type: 'video/mp4'
+        }],
+        poster: 'http://media.w3.org/2010/05/sintel/poster.png'
+      }, {
+        sources: [{
+          src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+          type: 'video/mp4'
+        }],
+        poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+      }, {
+        sources: [{
+          src: 'http://vjs.zencdn.net/v/oceans.mp4',
+          type: 'video/mp4'
+        }],
+        poster: 'http://www.videojs.com/img/poster.jpg'
+      }, {
+        sources: [{
+          src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+          type: 'video/mp4'
+        }],
+        poster: 'http://media.w3.org/2010/05/video/poster.png'
+      }])
+      
+      // Play through the playlist automatically.
+      player.playlist.autoadvance(0)
+
+    }
+  });
+
+  return (
+    <div>
+      <video controls ref={videoRef} className="video-js" />
+    </div>
+  )
+}
---
+++ src/app/video/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRef, useEffect } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+export default function Video() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videojs(videoRef.current, {
+        sources: [
+          {
+            src: "https://stream.mux.com/GJjLF93MGEmq4VfidIdZ4oMMAJRhEjSQ.m3u8",
+            type: "application/x-mpegURL"
+          }
+        ]
+      });
+    }
+  });
+
+  return (
+    <div>
+      <video controls ref={videoRef} className="video-js" />
+    </div>
+  )
+}
