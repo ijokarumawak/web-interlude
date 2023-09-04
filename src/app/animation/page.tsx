996c267026d267fa52de39f0ac59a7294883effa
Koji Kawamura <ijokarumawak@gmail.com>
2023-09-04 18:13:16 +0900

    Migrated pages to app router.

---
+++ src/app/animation/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import dynamic from "next/dynamic"
+
+const PixiApp = dynamic(() => import("../../components/PixiApp"), { ssr: false })
+
+export default function Anim() {
+  return (
+    <div>
+      <h1>Pixi App in NextJs</h1>
+      <PixiApp />
+    </div>
+  )
+}
