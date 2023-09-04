3251d5c1685f31aafc74a85bde0a464d226d9c39
Koji Kawamura <ijokarumawak@gmail.com>
2023-09-04 16:43:54 +0900

    http to https.

--- src/app/video-playlist/page.tsx
+++ src/app/video-playlist/page.tsx
@@ -17,28 +17,28 @@ export default function Video() {
 
       player.playlist([{
         sources: [{
-          src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+          src: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
           type: 'video/mp4'
         }],
-        poster: 'http://media.w3.org/2010/05/sintel/poster.png'
+        poster: 'https://media.w3.org/2010/05/sintel/poster.png'
       }, {
         sources: [{
-          src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+          src: 'https://media.w3.org/2010/05/bunny/trailer.mp4',
           type: 'video/mp4'
         }],
-        poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+        poster: 'https://media.w3.org/2010/05/bunny/poster.png'
       }, {
         sources: [{
-          src: 'http://vjs.zencdn.net/v/oceans.mp4',
+          src: 'https://vjs.zencdn.net/v/oceans.mp4',
           type: 'video/mp4'
         }],
-        poster: 'http://www.videojs.com/img/poster.jpg'
+        poster: 'https://www.videojs.com/img/poster.jpg'
       }, {
         sources: [{
-          src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+          src: 'https://media.w3.org/2010/05/video/movie_300.mp4',
           type: 'video/mp4'
         }],
-        poster: 'http://media.w3.org/2010/05/video/poster.png'
+        poster: 'https://media.w3.org/2010/05/video/poster.png'
       }])
       
       // Play through the playlist automatically.
