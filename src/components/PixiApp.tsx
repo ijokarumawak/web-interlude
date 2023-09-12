8f383e67d5ae34b2c3c0e90677f38c9312d15671
Koji Kawamura <ijokarumawak@gmail.com>
2023-09-12 17:15:36 +0900

    Added Pixi sounds.

--- src/components/PixiApp.tsx
+++ src/components/PixiApp.tsx
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import { Stage, Container, Sprite, useTick, Text } from '@pixi/react'
 import * as PIXI from "pixi.js"
+import { sound } from "@pixi/sound"
 import { ContentProperties } from './ContentProperties'
 
 const RotatingBunny: React.FC<ContentProperties> = (props:ContentProperties) => {
 
   const [count, setCount] = useState(10)
+  const [rotation, setRotation] = useState(0)
 
+  if(!sound.exists('bgm')) sound.add('bgm', '/sounds/jimit-big-beats.m4a')
+  if(!sound.exists('boing')) sound.add('boing', '/sounds/examples_resources_boing.mp3')
+  if(!sound.exists('chime')) sound.add('chime', '/sounds/examples_resources_chime.mp3')
+
+  // Load 時に bgm を開始
+  useEffect(() => {
+    console.log('once...')
+    sound.volume('bgm', 0.5)
+    sound.play('bgm')
+  }, [])
+
+  // 1秒ごとの処理
   useEffect(() => {
     if (count <= 0) {
+      sound.stop('bgm')
+      sound.play('chime')
       props.onEnded()
     } else {
+
+      if (count <= 5) {
+        sound.pause('boing')
+        sound.play('boing')
+      }
+
       setTimeout(() => {setCount(count - 1)}, 1000)  
     }
   }, [count])
 
-  const [rotation, setRotation] = useState(0)
+  // PIXI フレーム更新時の処理
+  useTick((delta) => {
+    // 開始直後、終了間際は静止
+    if ((rotation * (180/Math.PI) % 360 <= 10 && count <= 1)) {
+      setRotation(0)
+    } else {
+      delta && count < 9 && setRotation(rotation + 0.1 * delta)
+    }
 
-  useTick((delta) => delta && setRotation(rotation + 0.1 * delta))
+    // bgm フェードアウト
+    const volume = sound.volume('bgm')
+    if (count <= 5 && volume > 0) {
+      sound.volume('bgm', Math.max(volume - 0.002, 0))
+    }
+  })
 
+  
   const style = new PIXI.TextStyle({
     align: 'center',
     fontFamily: 'sans-serif',
@@ -42,6 +77,7 @@ const RotatingBunny: React.FC<ContentProperties> = (props:ContentProperties) => 
       />
       <Text
         text={count.toString()}
+        x={-20}
         y={150}
         style={style}
       />
