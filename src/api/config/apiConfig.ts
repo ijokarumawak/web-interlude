d045c87bcd4c0e9b57a304d50cf12c469cc71b18
tkc66 <worktkc2018@gmail.com>
2023-11-17 01:05:20 +0900

    fix: change endpoint to test instance

--- src/api/config/apiConfig.ts
+++ src/api/config/apiConfig.ts
@@ -1,5 +1,5 @@
 // eventAbbrはイベント毎に変更する.
 export const apiConfig = {
-    talksEndpoint: 'https://event.cloudnativedays.jp/api/v1/talks?eventAbbr=cndt2023',
-    speakersEndpoint: 'https://event.cloudnativedays.jp/api/v1/speakers?eventAbbr=cndt2023',
+    talksEndpoint: 'https://dreamkast-dk-2119.dev.cloudnativedays.jp/api/v1/talks?eventAbbr=cndt2023',
+    speakersEndpoint: 'https://dreamkast-dk-2119.dev.cloudnativedays.jp/api/v1/speakers?eventAbbr=cndt2023',
 };
