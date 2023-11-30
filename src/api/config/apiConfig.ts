f951cc9387d002a906fe54cf9a0b4faa47d5d1dd
Gaku-Kunimi <dummy@uminek.ooo>
2023-11-30 19:26:20 +0900

    localhost でもデバッグできるように 設定済みReviewAppに API の向き先を変えた

--- src/api/config/apiConfig.ts
+++ src/api/config/apiConfig.ts
@@ -1,8 +1,13 @@
 // eventAbbrはイベント毎に変更する.
 const eventAbbr = 'cndt2023';
 
+// export const apiConfig = {
+//     talksEndpoint: `https://event.cloudnativedays.jp/api/v1/talks?eventAbbr=${eventAbbr}`,
+//     talkEndpoint: `https://event.cloudnativedays.jp/api/v1/talks/`,
+//     speakersEndpoint:  `https://event.cloudnativedays.jp/api/v1/speakers?eventAbbr=${eventAbbr}`,
+// };
 export const apiConfig = {
-    talksEndpoint: `https://event.cloudnativedays.jp/api/v1/talks?eventAbbr=${eventAbbr}`,
-    talkEndpoint: `https://event.cloudnativedays.jp/api/v1/talks/`,
-    speakersEndpoint:  `https://event.cloudnativedays.jp/api/v1/speakers?eventAbbr=${eventAbbr}`,
+    talksEndpoint: `https://dreamkast-dk-2147.dev.cloudnativedays.jp/api/v1/talks?eventAbbr=${eventAbbr}`,
+    talkEndpoint: `https://dreamkast-dk-2147.dev.cloudnativedays.jp/api/v1/talks/`,
+    speakersEndpoint:  `https://dreamkast-dk-2147.dev.cloudnativedays.jp/api/v1/speakers?eventAbbr=${eventAbbr}`,
 };
