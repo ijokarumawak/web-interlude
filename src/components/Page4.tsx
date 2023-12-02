c799a901e0fd1f9068a1e21505b20cfae8c82838
Hiroki Okui <hiroki.okui@gmail.com>
2023-12-03 00:14:23 +0900

    fix: Revert playlist comment out

--- src/components/Page4.tsx
+++ src/components/Page4.tsx
@@ -15,30 +15,30 @@ const playlist: Playlist = [
       },
     ],
   },
-  // {
-  //   sources: [
-  //     {
-  //       src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm02-line.mp4',
-  //       type: 'video/mp4',
-  //     },
-  //   ],
-  // },
-  // {
-  //   sources: [
-  //     {
-  //       src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm03-mirantis.mp4',
-  //       type: 'video/mp4',
-  //     },
-  //   ],
-  // },
-  // {
-  //   sources: [
-  //     {
-  //       src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm04-legalforce.mp4',
-  //       type: 'video/mp4',
-  //     },
-  //   ],
-  // },
+  {
+    sources: [
+      {
+        src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm02-line.mp4',
+        type: 'video/mp4',
+      },
+    ],
+  },
+  {
+    sources: [
+      {
+        src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm03-mirantis.mp4',
+        type: 'video/mp4',
+      },
+    ],
+  },
+  {
+    sources: [
+      {
+        src: 'https://dreamkast-public-bucket.s3.ap-northeast-1.amazonaws.com/videos/cm/modified_cm04-legalforce.mp4',
+        type: 'video/mp4',
+      },
+    ],
+  },
 ]
 
 export default function Page(_: Props) {
