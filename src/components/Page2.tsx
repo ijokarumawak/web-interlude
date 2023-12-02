dca4504c71a360b7a2cfd3c260814eafe8f0a887
Hiroki Okui <hiroki.okui@gmail.com>
2023-12-03 00:03:30 +0900

    feat: Add Sample of page2

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -3,6 +3,8 @@ import { TalkView } from './models/talkView'
 import { useContext, useEffect } from 'react'
 import { PageCtx } from './models/pageContext'
 import config from '@/config'
+import { getTimeStr } from '@/utils/time'
+import { Talk, Track } from '@/generated/dreamkast-api.generated'
 
 type Props = { view: Optional<TalkView> }
 
@@ -13,27 +15,90 @@ export default function Page({ view }: Props) {
   }, [goNextPage])
 
   return (
-    <div className="max-w- mx-auto bg-white shadow-md overflow-hidden">
-      <div className="flex justify-between items-center bg-gray-200 p-4">
-        <div className="text-lg text-gray-700">2023/08/03</div>
-        <div className="text-2xl text-gray-800">PAGE 2</div>
+    <div>
+      <div className="h-[120px]">
+        <Header view={view} />
       </div>
 
-      <div className="p-6 border-b border-gray-300">
-        <div className="text-lg text-gray-600">18:00 - 18:40</div>
-        <div className="text-xl text-gray-800 my-2">テストのタイトル</div>
-        <div className="text-lg font-bold">Test Taro</div>
-        <div className="mt-4">
-          <div className="text-sm text-gray-600">N/A</div>
-          <div className="text-sm text-gray-600">
-            PoC(検証), Production(本番環境)
-          </div>
-          <div className="text-sm text-gray-600">
-            app-developer - アプリケーション開発, operator/sys-admin -
-            運用管理/システム管理
-          </div>
-        </div>
+      <div className="h-[600px]">
+        <Body view={view} />
       </div>
     </div>
   )
+}
+
+function Header({ view }: Props) {
+  const { now } = useContext(PageCtx)
+  if (!view) {
+    return <></>
+  }
+  const talk = view.selectedTalk
+  return (
+    <div className="flex flex-row items-center h-full text-gray-600 bg-gray-100">
+      <div className="basis-1/3">
+        <div className="text-2xl  text-center">Track {view.selectedTrack.name}</div>
+      </div>
+      <div className="basis-1/3">
+        <div className="text-lg text-center">{config.eventAbbr.toUpperCase()}</div>
+        <div className="text-3xl text-center">{now.format('HH:mm:ss')}</div>
+      </div>
+      <div className="basis-1/12">
+        <div className="text-xl text-center">Next</div>
+      </div>
+      <div className="basis-1/4 pr-4">
+        <div className="text-lg text-center">{getTimeStr(talk.startTime)} - {getTimeStr(talk.endTime)}</div>
+        <div className="text-lg text-center">{trim(talk.title, 40)}</div>
+      </div>
+    </div>
+  )
+}
+
+function Body({ view }: Props) {
+  if (!view) {
+    return <></>
+  }
+  const talk = view.selectedTalk
+  const nextTalks = view.talksInNextSlot()
+  return (
+    <div className="py-6">
+      <div className="text-left w-[450px] bg-sky-500 pr-2 py-6">
+        <div className="text-right text-white font-bold font-['Open_Sans'] text-3xl">UPCOMING SESSION</div>
+      </div>
+      <div className="grid grid-cols-2 gap8">
+        {view.allTracks.map(track => {
+          const talk = nextTalks[track.name]
+          return (
+            <Track key={track.id} talk={talk} track={track} />
+          )
+        })}
+      </div> 
+    </div>
+  )
+}
+
+type TrackProps = {
+  talk: Talk
+  track: Track
+}
+
+function Track({ talk, track }: TrackProps) {
+  if (!talk || !track) {
+    return <></>
+  }
+  return (
+    <div className="flex flex-row items-center text-gray-600 w-[600px] h-[250px]">
+      <div className="basis-1/3">
+        <img src={"/cndt2023/trademark.png"} className="w-[120px] h-[120px] ml-auto mr-5"/>
+      </div>
+      <div className="basis-2/3">
+        <div className="text-2xl my-3">Track {track.name}</div>
+        <div className="text-xl my-3">{talk.speakers.map(s => s.name).join(", ")}</div>
+        <div className="text-xl my-3">{talk.title}</div>
+      </div>
+    </div>
+  )
+}
+
+function trim(str: string, len: number) {
+  return str.length > len ? str.substring(0, len - 3) + '...' : str
 }
